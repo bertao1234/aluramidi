0f50edf98efb20cdbe74b388923348bc1c7530c9
bertao1234 <126819816+bertao1234@users.noreply.github.com>
2023-10-26 17:56:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-document.querySelector('.tecla_pom');
+Function tocaSomPom() { 
+  document.querySelector('#som_tecla_pom').play(); 
+} 
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+
+Function tocaSomClap() { 
+    document.querySelector('#som_tecla_clap').play(); 
+  } 
+  
+  document.querySelector('.tecla_clap').onclick = tocaSomClap;
+   
+  Function tocaSomTim() { 
+    document.querySelector('#som_tecla_tim').play(); 
+  } 
+  
+  document.querySelector('.tecla_tim').onclick = tocaSomTim;
